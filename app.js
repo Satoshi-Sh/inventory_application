7118c48ea17526faf735cb919cdefb79e88f98e1
Satoshi-Sh <satoss1108@gmail.com>
2022-11-16 10:25:37 -0600

    Added .env for security key

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = express();
 const mongoose = require("mongoose");
 
 
-const mongoDB = process.env.Key 
+const mongoDB = process.env.KEY
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
